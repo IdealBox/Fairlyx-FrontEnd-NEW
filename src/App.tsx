84257ea5b6d3de2c735338082dad9f4387c1b932
ideal-lovette <kimbohlovette@idealbox.org>
2023-10-02 13:55:34 +0100

    cleaned unused files, configured redux toolkit, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useAppSelector } from './store/hooks';
+
+const MyApp = () => {
+	const isAuthenticated = useAppSelector(
+		(state) => state.user.isAuthenticated
+	);
+	return (
+		<div>
+			{isAuthenticated ? (
+				<p className="text-green-400">Authenticated</p>
+			) : (
+				<p className="text-red-400">Not authenticated</p>
+			)}
+			<h1 className="text-blue-500 font-bold font-inter text-4xl">
+				Welcome to fairlyx
+			</h1>
+		</div>
+	);
+};
+
+const routes = createBrowserRouter([
+	{
+		path: '/',
+		element: <MyApp />,
+		children: [
+			// All route configurations go here
+		],
+	},
+]);
 
 const App = () => {
-  return (<h1>Welcome to fairlyx</h1>);
-}
- 
+	return <RouterProvider router={routes} />;
+};
+
 export default App;
-
-
-
