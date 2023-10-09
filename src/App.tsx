55abdd07127dd5d1722099436bb562079ed31716
ideal-lovette <145937652+ideal-lovette@users.noreply.github.com>
2023-10-09 08:06:06 +0100

    Flx 26 implement product views (#14)
    * Properly configured routes in app and exposed Outlet in MyApp.tsx
    
    * removed route with notused overview child
    
    * Product views  completed
    
    * Removed unused imports and variables
    
    * Navbar modals (#12)
    
    * init
    
    * modals done
    
    ---------
    
    Co-authored-by: casaemiro <silasmagho18@gmail.com>
    
    ---------
    
    Co-authored-by: AkonehSilas <145991697+AkonehSilas@users.noreply.github.com>
    Co-authored-by: casaemiro <silasmagho18@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from './store/hooks';
 import { setToDarkMode, setToLightMode } from './store/slices/appThemeSlice';
 import { DashboardHome } from './screens';
 import Authentication from './screens/authentication';
+import Overview from './components/Overview';
+import ProductViews from './components/ProductViews';
 
 const MyApp = () => {
 	const dispatch = useAppDispatch();
@@ -25,7 +27,9 @@ const MyApp = () => {
 
 	return (
 		<main className={`font-inter ${isDarkMode ? 'dark' : ''}`}>
-			<div className="bg-app-neutral-100 dark:bg-app-neutral-800 min-h-screen"></div>
+			<div className="bg-app-neutral-100 dark:bg-app-neutral-800 min-h-screen">
+				<Outlet />
+			</div>
 		</main>
 	);
 };
@@ -34,17 +38,24 @@ const routes = createBrowserRouter([
 	{
 		path: '/',
 		element: <MyApp />,
-		children: [],
-	},
-	{
-		path: '/dashboard',
-		element: <DashboardHome />,
-		children: [],
-	},
-	{
-		path: '/authentication',
-		element: <Authentication />,
-		children: [],
+		children: [
+			{
+				path: '/dashboard',
+				element: <DashboardHome />,
+				children: [
+					// all pages in the dashboard can go here
+					{
+						path: '',
+						element: <ProductViews />,
+					},
+				],
+			},
+			{
+				path: '/authentication',
+				element: <Authentication />,
+				children: [],
+			},
+		],
 	},
 ]);
 
