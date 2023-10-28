727994f9ea7ee3c2326bc1f1522175a80dd61ddc
ideal-lovette <145937652+ideal-lovette@users.noreply.github.com>
2023-10-28 04:37:00 +0100

    Implement product stat tabs (#24)
    * Implemented the product activity section
    
    * Implemented the product activity section
    
    * Improved the ProductStatsTable to accumulate market, traffic_resources and viewers stats
    
    * Improved the menu item component  with hover effects
    
    * Completed implementation of navigation between market, traffic-sources and viewers tap

--- src/screens/vendor/products/components/Dashboard.tsx
+++ src/screens/vendor/products/components/Dashboard.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ProductStats from './ProductStats';
 import ProductOverview from './ProductOverview';
+import ProductActivity from './ProductActivity';
 
 const Dashboard = () => {
-	return <div className="overflow-scroll gap-3 flex-col flex p-5">
-		<div className="font-bold text-5xl dark:text-white">Product dashboard</div>
-		<ProductOverview />
-		<ProductStats />
-	</div>;
+	return (
+		<div className="overflow-scroll gap-3 flex-col flex p-5">
+			<ProductOverview />
+			<div className="flex items-center">
+				<div className="flex-1">
+					<ProductActivity />
+				</div>
+			</div>
+			<ProductStats />
+		</div>
+	);
 };
 
 export default Dashboard;
