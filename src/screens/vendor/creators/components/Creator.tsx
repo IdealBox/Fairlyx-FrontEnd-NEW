929e6d951dd8b997fe9451553b8b03dd859c10e2
ideal-lovette <kimbohlovette@idealbox.org>
2023-11-17 23:35:49 +0100

    explore creators completed for light mode

--- src/screens/vendor/creators/components/Creator.tsx
+++ src/screens/vendor/creators/components/Creator.tsx
@@ -1,27 +1,69 @@
 import React from 'react';
 import Avatar from '../../../../components/Avatar';
-
-const Creator = () => {
+import Slider from 'react-slick';
+import { BsArrowRightCircleFill } from 'react-icons/bs';
+interface Props {
+	index: number;
+}
+const Creator = ({ index }: Props) => {
 	return (
 		<div>
-			<div className="flex items-center gap-2">
-				<Avatar
-					size={64}
-					imageSrc="https://images.unsplash.com/photo-1530268729831-4b0b9e170218?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-					nameInitials={'KL'}
-				/>
-				<div>
-					<h2 className="font-bold text-gray-800">
-						Reed Parker
-						<span className="py-1 px-1.5 ml-2 bg-secondary-4 rounded-md">
-							#1
-						</span>
-					</h2>
-					<p className="text-sm text-gray-500 py-1.5">
-						<span className="font-semibold">16</span> products
-						<span className="px-2 text-gray-400 text-xs">|</span>
-						<span className="font-semibold">3568</span> followers
-					</p>
+			<div className="flex items-center justify-between gap-4">
+				<div className="flex items-center gap-2">
+					<Avatar
+						size={64}
+						imageSrc="https://images.unsplash.com/photo-1530268729831-4b0b9e170218?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+						nameInitials={'KL'}
+					/>
+					<div>
+						<h2 className="font-bold text-gray-800">
+							Reed Parker
+							<span className="py-1 px-1.5 ml-2 bg-secondary-4 rounded-md">
+								#{index}
+							</span>
+						</h2>
+
+						<p className="text-sm text-gray-500 py-1.5">
+							<span className="font-semibold">16</span> products
+							<span className="px-2 text-gray-400 text-xs">
+								|
+							</span>
+							<span className="font-semibold">3568</span>{' '}
+							followers
+						</p>
+					</div>
+				</div>
+				<div className="hidden sm:flex items-center gap-2">
+					<button className="py-2 px-4 rounded-lg bg-slate-200 border hover:shadow font-semibold text-gray-800 text-xs">
+						Follow
+					</button>
+					<button className="hidden lg:block py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-600 font-semibold text-white text-xs">
+						Shop now
+					</button>
+				</div>
+			</div>
+			<div>
+				<div className="w-full overflow-auto scrollbar-thin my-5">
+					<div className="flex items-center gap-4">
+						{Array(5)
+							.fill(3)
+							.map((item, idx) => (
+								<div
+									key={idx}
+									className="group relative h-48 aspect-[9/7]"
+								>
+									<div className="hidden group-hover:grid absolute top-0 h-full left-0 w-full place-items-center bg-slate-900/60 rounded-xl">
+										<div className="text-white text-3xl">
+											<BsArrowRightCircleFill />
+										</div>
+									</div>
+									<img
+										className="w-full h-full object-center object-cover rounded-xl"
+										src="https://images.unsplash.com/photo-1530268729831-4b0b9e170218?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+									/>
+								</div>
+							))}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/screens/vendor/creators/index.tsx
+++ src/screens/vendor/creators/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Creator from './components/Creator';
 import Dropdown from '../../../components/forms/Dropdown';
+import { LuLoader } from 'react-icons/lu';
 
 const CreatorsPage = () => {
 	const [selectedValue, setSelectedValue] = useState<{
@@ -12,16 +13,38 @@ const CreatorsPage = () => {
 		id: '1',
 		type: '',
 	});
+
+	const [tab, setTab] = useState<'popular' | 'trending'>('popular');
 	return (
 		<div className="px-4 py-8">
 			<h1 className="leading-loose text-4xl font-semibold text-gray-900 dark:text-gray-50">
 				Explore creators
 			</h1>
 			<div className="bg-white rounded-md p-4">
-				<header className="border-b border-gray-200 py-4 flex flex-col">
+				<header className="border-b border-gray-200 py-4 flex flex-col sm:flex-row sm:justify-between sm:items-center">
 					<div className="flex flex-row [&>*]:flex-1 text-center text-sm text-gray-600 font-medium mb-5">
-						<div className="py-2 px-4 rounded-md">Popular</div>
-						<div className="py-2 px-4 bg-gray-200 rounded-md">
+						<div
+							onClick={() => {
+								if (tab !== 'popular') {
+									setTab('popular');
+								}
+							}}
+							className={`py-2 px-4 rounded-md ${
+								tab === 'popular' ? '' : 'bg-gray-200'
+							}`}
+						>
+							Popular
+						</div>
+						<div
+							onClick={() => {
+								if (tab !== 'trending') {
+									setTab('trending');
+								}
+							}}
+							className={`py-2 px-4 rounded-md ${
+								tab === 'trending' ? '' : 'bg-gray-200'
+							}`}
+						>
 							Trending
 						</div>
 					</div>
@@ -46,8 +69,18 @@ const CreatorsPage = () => {
 					<p className="text-xs font-semibold text-gray-400">
 						Viewing 5 of 5000+ creators in the market
 					</p>
-					<Creator />
-					<Creator />
+					<div className="divide-y flex flex-col gap-4 [&>*]:pt-4">
+						{Array(5)
+							.fill(3)
+							.map((item, idx) => {
+								return <Creator index={idx + 1} />;
+							})}
+					</div>
+
+					<button className="py-2 px-4 rounded-lg bg-slate-200 border border-gray-200 hover:shadow font-semibold text-gray-800 text-xs w-fit mx-auto">
+						<LuLoader className="inline" />
+						<span className="pl-2">Load more</span>
+					</button>
 				</div>
 			</div>
 		</div>
