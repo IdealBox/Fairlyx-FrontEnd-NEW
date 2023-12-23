eab5199615286cab2f63c85499deb11f2aea14bf
ideal-lovette <kimbohlovette@idealbox.org>
2023-12-23 20:58:22 +0100

    new customer's layout done

---
+++ src/screens/vendor/customers/components/NewCustomers.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewCustomers = () => {
+	return (
+		<div className="p-4 bg-white rounded-lg">
+			<header className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+				<div>
+					<h1 className="text-gray-800 dark:text-app-neutral-50 text-lg font-semibold before:content-[''] before:w-4 before:aspect-[2/4] before:bg-red-200 before:rounded-sm flex items-center gap-2">
+						New customers
+					</h1>
+				</div>
+			</header>
+		</div>
+	);
+};
+
+export default NewCustomers;
